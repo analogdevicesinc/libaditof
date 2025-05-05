928b04b04794a87bcac9bcd6972923e271dabd34
Andre Straker <andre.straker@analog.com>
2025-05-05 17:14:12 +0000

    :

--- sdk/src/connections/target/adsd3500_sensor.cpp
+++ sdk/src/connections/target/adsd3500_sensor.cpp
@@ -734,7 +734,7 @@ Adsd3500Sensor::setMode(const aditof::DepthSensorModeDetails &type) {
     return status;
 }
 
-aditof::Status Adsd3500Sensor::getFrame(uint16_t *buffer) {
+aditof::Status Adsd3500Sensor::getFrame(uint16_t *buffer, uint32_t index) {
 
     using namespace aditof;
     Status status;
@@ -2133,3 +2133,234 @@ aditof::Status Adsd3500Sensor::getIniParamsArrayForMode(int mode,
 
     return Status::OK;
 }
+
+#pragma region Stream_Recording_and_Playback
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#ifdef _WIN32
+#include <direct.h>
+#endif
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <sstream>
+
+static bool folderExists(const std::string &path) {
+    struct stat info;
+    return (stat(path.c_str(), &info) == 0 && (info.st_mode & S_IFDIR));
+}
+
+static bool createFolder(const std::string &path) {
+#ifdef _WIN32
+    return _mkdir(path.c_str()) == 0 || errno == EEXIST;
+#else
+    return mkdir(path.c_str(), 0755) == 0 || errno == EEXIST;
+#endif
+}
+
+static std::string generateFileName(const std::string &prefix = "aditof_",
+                                    const std::string &extension = ".adcam") {
+    // Get current UTC time
+    std::time_t now = std::time(nullptr);
+    std::tm utc_tm;
+#ifdef _WIN32
+    gmtime_s(&utc_tm, &now); // Windows
+#else
+    gmtime_r(&now, &utc_tm); // Linux/macOS
+#endif
+
+    std::ostringstream oss;
+
+    // Format time: YYYYMMDD_HHMMSS
+    oss << prefix;
+    oss << std::put_time(&utc_tm, "%Y%m%d_%H%M%S");
+
+    // Generate random 8-digit hex
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<uint32_t> dis(0, 0xFFFFFFFF);
+    uint32_t randNum = dis(gen);
+    oss << "_" << std::hex << std::setw(8) << std::setfill('0') << randNum;
+
+    oss << extension;
+
+    return oss.str();
+}
+
+aditof::Status Adsd3500Sensor::startRecording(std::string &fileName,
+                                                  uint8_t *parameters,
+    uint32_t paramSize) {
+
+    using namespace aditof;
+
+    Status status = Status::GENERIC_ERROR;
+
+    return status;
+}
+
+aditof::Status Adsd3500Sensor::stopRecording() {
+
+    using namespace aditof;
+
+    Status status = Status::GENERIC_ERROR;
+
+    return status;
+}
+
+aditof::Status Adsd3500Sensor::startPlayback(const std::string filePath) {
+    m_state = ST_STANDARD;
+
+    if (m_stream_file_in.is_open()) {
+        m_stream_file_in.close();
+    }
+
+    m_frameIndex.clear();
+    m_stream_file_in = std::ifstream(filePath, std::ios::binary);
+
+    m_state = ST_PLAYBACK;
+
+    return aditof::Status::OK;
+}
+
+aditof::Status Adsd3500Sensor::stopPlayback() {
+    m_state = ST_STANDARD;
+    if (m_stream_file_in.is_open()) {
+        m_frameIndex.clear();
+        m_stream_file_in.close();
+        return aditof::Status::OK;
+    }
+    LOG(ERROR) << "File stream is not open";
+    return aditof::Status::GENERIC_ERROR;
+}
+
+aditof::Status Adsd3500Sensor::getFrameCount(uint32_t &frameCount) {
+    frameCount = m_frameIndex.size();
+
+    return aditof::Status::OK;
+}
+
+aditof::Status Adsd3500Sensor::getHeader(uint8_t* buffer, uint32_t bufferSize) {
+
+        if (m_state != ST_PLAYBACK) {
+        LOG(ERROR) << "Not in playback mode";
+        return aditof::Status::GENERIC_ERROR;
+    }
+
+    try {
+        if (m_stream_file_in.is_open()) {
+
+            m_stream_file_in.seekg(0, std::ios::beg);
+
+            uint32_t x;
+            m_stream_file_in.read(reinterpret_cast<char *>(&x), sizeof(x));
+
+            // Read size of buffer
+            uint32_t _bufferSize = 0;
+            m_stream_file_in.read(reinterpret_cast<char *>(&_bufferSize),
+                                  sizeof(_bufferSize));
+
+            bufferSize = _bufferSize;
+
+            // Read buffer data
+            m_stream_file_in.read(reinterpret_cast<char *>(buffer), bufferSize);
+
+            m_frameIndex.clear();
+
+            std::streampos pos = m_stream_file_in.tellg();
+
+            static uint32_t idx = 0;
+            LOG(INFO) << "Building Index";
+            while (true) {
+
+                uint32_t _bufferSize = 0;
+
+                pos = m_stream_file_in.tellg();
+
+                uint32_t x;
+                m_stream_file_in.read(reinterpret_cast<char *>(&x), sizeof(x));
+
+                if (m_stream_file_in.eof()) {
+                    break;
+                }
+
+                m_stream_file_in.read(reinterpret_cast<char *>(&_bufferSize),
+                                      sizeof(_bufferSize));
+
+                if (m_stream_file_in.eof()) {
+                    break;
+                }
+
+                m_frameIndex.emplace_back(pos);
+
+                m_stream_file_in.seekg(_bufferSize, std::ios::cur);
+            }
+
+            m_stream_file_in.clear();
+
+            m_stream_file_in.seekg(pos, std::ios::beg);
+
+            return aditof::Status::OK;
+        }
+    } catch (const std::ofstream::failure &e) {
+        LOG(ERROR) << "File I/O exception caught: " << e.what();
+        m_stream_file_in.close();
+        m_state = ST_STANDARD;
+    }
+    return aditof::Status::GENERIC_ERROR;
+}
+
+aditof::Status Adsd3500Sensor::readFrame(uint8_t *buffer,
+                                             uint32_t &bufferSize, uint32_t index) {
+
+    if (m_state != ST_PLAYBACK) {
+        LOG(ERROR) << "Not in playback mode";
+        return aditof::Status::GENERIC_ERROR;
+    }
+
+    try {
+        if (m_stream_file_in.is_open()) {
+
+            m_stream_file_in.clear();
+
+            m_stream_file_in.seekg(m_frameIndex[index], std::ios::beg); 
+
+            auto p = m_stream_file_in.tellg();
+
+            uint32_t x;
+            m_stream_file_in.read(reinterpret_cast<char *>(&x),
+                                  sizeof(x));
+
+            // Read size of buffer
+            uint32_t _bufferSize = 0;
+            m_stream_file_in.read(reinterpret_cast<char *>(&_bufferSize),
+                                  sizeof(_bufferSize));
+
+            bufferSize = _bufferSize;
+
+            // Read buffer data
+            m_stream_file_in.read(reinterpret_cast<char *>(buffer), bufferSize);
+
+            return aditof::Status::OK;
+        }
+    } catch (const std::ofstream::failure &e) {
+        LOG(ERROR) << "File I/O exception caught: " << e.what();
+        m_stream_file_in.close();
+        m_state = ST_STANDARD;
+    }
+    return aditof::Status::GENERIC_ERROR;
+}
+
+aditof::Status Adsd3500Sensor::automaticStop() {
+    if (m_state == ST_PLAYBACK) {
+        stopPlayback();
+    } else if (m_state == ST_RECORD) {
+        stopRecording();
+    }
+
+    return aditof::Status::OK;
+}
+
+#pragma endregion
